e6d2104b95cda621095a7e25184c6eb5736c67e5
javierWebDev <151965097+JavierWebDev@users.noreply.github.com>
2024-08-01 08:14:14 -0500

    feat: :tada: Implementing Hex Architecture
    Started the proyect, implemented the folder structure and hexagonal architecture

---
+++ proyectospringboot/src/test/java/com/campuslands/proyectospringboot/ProyectospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuslands.proyectospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
