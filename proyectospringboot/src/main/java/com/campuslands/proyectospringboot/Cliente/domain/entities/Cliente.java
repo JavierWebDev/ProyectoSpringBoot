bbc949c0e89bf88ff67a2f86b79dd0728cc699f8
javierWebDev <151965097+JavierWebDev@users.noreply.github.com>
2024-08-12 05:41:32 -0500

    feat: :rocket: Work

--- proyectospringboot/src/main/java/com/campuslands/proyectospringboot/Cliente/domain/entities/Cliente.java
+++ proyectospringboot/src/main/java/com/campuslands/proyectospringboot/Cliente/domain/entities/Cliente.java
@@ -8,7 +8,9 @@ import com.campuslands.proyectospringboot.Contacto.domain.Contacto;
 import com.campuslands.proyectospringboot.Direccion.domain.entities.Direccion;
 import com.campuslands.proyectospringboot.Empleado.domain.entities.Empleado;
 import com.campuslands.proyectospringboot.Fax.domain.entities.Fax;
+import com.campuslands.proyectospringboot.Pago.domain.entities.Pago;
 import com.campuslands.proyectospringboot.Pais.domain.entities.Pais;
+import com.campuslands.proyectospringboot.Pedido.domain.entities.Pedido;
 import com.campuslands.proyectospringboot.Telefono.domain.entitie.Telefono;
 
 import jakarta.persistence.*;
@@ -85,4 +87,8 @@ public class Cliente {
 
     @OneToMany(mappedBy = "cliente", cascade= CascadeType.ALL, orphanRemoval = true)
     private Set<Pedido> pedido;
+
+    @OneToMany(mappedBy = "cliente", cascade= CascadeType.ALL, orphanRemoval = true)
+    private Set<Pago> pago;
 }
+
